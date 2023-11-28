1e168ae4fb070f534b0277243d2e2d0ab0ab0c2f
JULIUS NYERERE <nyerereoffice@gmail.com>
2023-11-28 22:36:11 +0300

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+    
+# from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
+
+from .forms import UserRegisterForm
+
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        
+        
+        if form.is_valid():
+            
+            
+            form.save()
+
+            
+            username = form.cleaned_data.get('username')
+            
+            messages.success(request, f'Your account has been created! Log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+        
+    context = {'form': form}
+    print(form.errors)
+    return render(request, 'users/register.html', context)
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
